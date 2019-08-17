7b96d5582399634b673dbff641a233ba6c509a8f
Sebastián Alarcón <sebastian.alarcon.beltran@gmail.com>
2019-08-17 15:32:48 -0400

    Pantalla de inicio preeliminar y 2 audios

---
+++ Assets/Scripts/pantallaInicio.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class pantallaInicio : MonoBehaviour {
+
+	public GameObject modelo;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		modelo.transform.Rotate(0,50*Time.deltaTime,0);
+	}
+
+	public void ComenzarPartida(){
+		SceneManager.LoadScene("test01");
+	}
+
+	public void VerCreditos(){
+
+	}
+	
+	public void VerInstrucciones(){
+
+	}
+}
