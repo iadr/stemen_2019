b66e09b9eaeb552063780320003b494c7cce6fc8
Sebastián Alarcón <sebastian.alarcon.beltran@gmail.com>
2019-08-17 23:36:19 -0400

    Pantalla Instrucciones y Créditos

--- Assets/Scripts/pantallaInicio.cs
+++ Assets/Scripts/pantallaInicio.cs
@@ -1,27 +1,61 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class pantallaInicio : MonoBehaviour {
 
 	public GameObject modelo;
+	public GameObject CanvasPantallaTitulo;
+	public GameObject CanvasPantallaInstrucciones;
+	public GameObject botonInstrucciones;
+	private bool volver = false;
+	private bool boolinst = false;
+	public GameObject CanvasPantallaCreditos;
 	void Start () {
-		
+		CanvasPantallaTitulo.SetActive(true);
+		CanvasPantallaInstrucciones.SetActive(false);
+		CanvasPantallaCreditos.SetActive(false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		modelo.transform.Rotate(0,50*Time.deltaTime,0);
+
+		if (Input.GetButtonDown("Fire2") && volver){
+			VerPantallaTitulo();
+			if (boolinst){
+				EventSystem.current.SetSelectedGameObject(botonInstrucciones);
+				boolinst = false;
+			}
+		}
 	}
 
 	public void ComenzarPartida(){
 		SceneManager.LoadScene("test01");
 	}
 
-	public void VerCreditos(){
+	public void VerPantallaTitulo(){
+		CanvasPantallaTitulo.SetActive(true);
+		CanvasPantallaInstrucciones.SetActive(false);
+		CanvasPantallaCreditos.SetActive(false);
+		volver = false;
+	}
 
+	public void VerCreditos(){
+		CanvasPantallaTitulo.SetActive(false);
+		CanvasPantallaCreditos.SetActive(true);
+		volver = true;
 	}
 	
 	public void VerInstrucciones(){
+		Debug.Log("instrucciones");
+		CanvasPantallaTitulo.SetActive(false);
+		CanvasPantallaInstrucciones.SetActive(true);
+		volver = true;
+		boolinst = true;
+	}
+
+	public void SalirPartida(){
 
 	}
 }
